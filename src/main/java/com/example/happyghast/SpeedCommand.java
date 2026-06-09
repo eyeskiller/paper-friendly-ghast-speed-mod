20c94c64db7bc4fecde5e9d35c7ea41171406004
Lubos Beran <lubosberan@Luboss-MacBook-Air.local>
2026-06-09 12:28:50 +0200

    Fix ChatColor deprecation by migrating to Kyori Adventure API

--- src/main/java/com/example/happyghast/SpeedCommand.java
+++ src/main/java/com/example/happyghast/SpeedCommand.java
@@ -1,6 +1,7 @@
 package com.example.happyghast;
 
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,13 +17,16 @@ public class SpeedCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!sender.hasPermission("happyghast.speed")) {
-            sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+            sender.sendMessage(Component.text("You don't have permission to use this command.").color(NamedTextColor.RED));
             return true;
         }
 
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.YELLOW + "Current Happy Ghast speed: " + ChatColor.WHITE + plugin.getGlobalSpeed());
-            sender.sendMessage(ChatColor.YELLOW + "Usage: /" + label + " <number> OR /" + label + " <increase|decrease> <number>");
+            sender.sendMessage(
+                    Component.text("Current Happy Ghast speed: ").color(NamedTextColor.YELLOW)
+                            .append(Component.text(plugin.getGlobalSpeed()).color(NamedTextColor.WHITE)));
+            sender.sendMessage(
+                    Component.text("Usage: /" + label + " <number> OR /" + label + " <increase|decrease> <number>").color(NamedTextColor.YELLOW));
             return true;
         }
 
@@ -31,7 +35,7 @@ public class SpeedCommand implements CommandExecutor {
                 // /happyghastspeed <number>
                 double newSpeed = Double.parseDouble(args[0]);
                 plugin.setGlobalSpeed(newSpeed);
-                sender.sendMessage(ChatColor.GREEN + "Happy Ghast speed set to: " + newSpeed);
+                sender.sendMessage(Component.text("Happy Ghast speed set to: " + newSpeed).color(NamedTextColor.GREEN));
             } else if (args.length == 2) {
                 // /happyghastspeed <increase|decrease> <number>
                 String action = args[0].toLowerCase();
@@ -40,18 +44,18 @@ public class SpeedCommand implements CommandExecutor {
 
                 if (action.equals("increase") || action.equals("add")) {
                     plugin.setGlobalSpeed(currentSpeed + amount);
-                    sender.sendMessage(ChatColor.GREEN + "Happy Ghast speed increased by " + amount + " to: " + plugin.getGlobalSpeed());
+                    sender.sendMessage(Component.text("Happy Ghast speed increased by " + amount + " to: " + plugin.getGlobalSpeed()).color(NamedTextColor.GREEN));
                 } else if (action.equals("decrease") || action.equals("reduce")) {
                     plugin.setGlobalSpeed(Math.max(0, currentSpeed - amount));
-                    sender.sendMessage(ChatColor.GREEN + "Happy Ghast speed decreased by " + amount + " to: " + plugin.getGlobalSpeed());
+                    sender.sendMessage(Component.text("Happy Ghast speed decreased by " + amount + " to: " + plugin.getGlobalSpeed()).color(NamedTextColor.GREEN));
                 } else {
-                    sender.sendMessage(ChatColor.RED + "Unknown action. Use 'increase' or 'decrease'.");
+                    sender.sendMessage(Component.text("Unknown action. Use 'increase' or 'decrease'.").color(NamedTextColor.RED));
                 }
             } else {
-                sender.sendMessage(ChatColor.RED + "Too many arguments. Usage: /" + label + " <number> OR /" + label + " <increase|decrease> <number>");
+                sender.sendMessage(Component.text("Too many arguments. Usage: /" + label + " <number> OR /" + label + " <increase|decrease> <number>").color(NamedTextColor.RED));
             }
         } catch (NumberFormatException e) {
-            sender.sendMessage(ChatColor.RED + "Invalid number provided.");
+            sender.sendMessage(Component.text("Invalid number provided.").color(NamedTextColor.RED));
         }
 
         return true;
